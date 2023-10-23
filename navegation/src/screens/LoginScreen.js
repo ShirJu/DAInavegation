eec9455928dbac2e8091482f2cb7564aacdbc236
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-23 10:43:31 -0300

    fin

--- navegation/src/screens/LoginScreen.js
+++ navegation/src/screens/LoginScreen.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Video, ResizeMode } from 'expo-av';
 import 'react-native-gesture-handler';
 
-export default function LoginScreen({ navigation }) {
+export default function LoginScreen() {
   const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+
+/*
+source={{
+          uri: require ("./../../assets/videos/big_buck_bunny.mp4")
+        }}
+*/
+
   return (
+    //a traves de un url local se reproduce el video
     <View style={styles.container}>
       <Video
         ref={video}
         style={styles.video}
-        source={{
-          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-        }}
+        source={require ("./../../assets/videos/big_buck_bunny.mp4")}
         useNativeControls
         resizeMode={ResizeMode.CONTAIN}
         isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
       />
     </View>
+    /*
+
+<View style={styles.container}>
+<Video
+  ref={video}
+  style={styles.video}
+  source={{
+    uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+  }}
+  useNativeControls
+  resizeMode={ResizeMode.CONTAIN}
+  isLooping
+  onPlaybackStatusUpdate={status => setStatus(() => status)}
+/>
+</View>*/
   );
 }
 
